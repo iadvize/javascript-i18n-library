a2e0bab8928be9be92ccbcf107458e4096e4bc70
Pierre-Alexandre Gury <pierre-alexandre.gury@iadvize.com>
2015-09-30 17:51:38 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# js-i18n-library
+
+> This javascript library is used to format dates, numbers and currencies. It's compatible with Node and Browsers.
+
+## Installation
+``` sh
+npm install js-i18n-library --save
+
+## Configuration
+
+The factory accept a config object to override default configuration.
+``` json
+{
+    referenceTimezone: 'Europe/Paris', // timezone used when no timezone is defined on the date to parse 
+    targetTimezone: 'Europe/Paris',    // timezone used to format the date
+    locale: 'fr-FR',                   // locale used to format numbers, currencies and long date format
+    shortDateFormat: 'DD/MM/YYYY',
+    longDateFormat: 'LL',
+    shortTimeFormat: 'HH:mm',
+    longTimeFormat: 'HH:mm:ss'
+}
+```
+
+## Usage Node / Browserify
+``` javascript
+var i18nServiceFactory = require('js-i18n-library');
+var config = {
+    referenceTimezone: 'Europe/Paris',
+    targetTimezone: 'Europe/Paris',
+    locale: 'fr-FR',
+    shortDateFormat: 'DD/MM/YYYY',
+    longDateFormat: 'LL',
+    shortTimeFormat: 'HH:mm',
+    longTimeFormat: 'HH:mm:ss'
+};
+
+var i18nService = i18nServiceFactory(config);
+var dateToFormat = "1990-11-26T23:21:00";
+
+// Dates
+i18nService.formatDate(dateToFormat);                            // 26/11/1990
+i18nService.formatDate(dateToFormat, i18nService.formats.SHORT); // 26/11/1990
+i18nService.formatDate(dateToFormat, i18nService.formats.LONG);  // 26 novembre 1990
+
+// Time
+i18nService.formatTime(dateToFormat);                            // 23:21
+i18nService.formatDate(dateToFormat, i18nService.formats.SHORT); // 23:21
+i18nService.formatDate(dateToFormat, i18nService.formats.LONG);  // 23:21:00
+
+// DateTime
+i18nService.formatDateTime(dateToFormat);                            // 26/11/1990 23:21
+i18nService.formatDateTime(dateToFormat, i18nService.formats.SHORT); // 26/11/1990 23:21
+i18nService.formatDateTime(dateToFormat, i18nService.formats.LONG);  // 26 novembre 1990 23:21:00
+```
+
+## Usage Browser
+``` javascript
+var config = {
+    referenceTimezone: 'Europe/Paris',
+    targetTimezone: 'Europe/Paris',
+    locale: 'fr-FR',
+    shortDateFormat: 'DD/MM/YYYY',
+    longDateFormat: 'LL',
+    shortTimeFormat: 'HH:mm',
+    longTimeFormat: 'HH:mm:ss'
+};
+
+var i18nService = window.iadvize.i18nServiceFactory(config);
+var dateToFormat = "1990-11-26T23:21:00";
+
+// Dates
+i18nService.formatDate(dateToFormat);                            // 26/11/1990
+i18nService.formatDate(dateToFormat, i18nService.formats.SHORT); // 26/11/1990
+i18nService.formatDate(dateToFormat, i18nService.formats.LONG);  // 26 novembre 1990
+
+// Time
+i18nService.formatTime(dateToFormat);                            // 23:21
+i18nService.formatDate(dateToFormat, i18nService.formats.SHORT); // 23:21
+i18nService.formatDate(dateToFormat, i18nService.formats.LONG);  // 23:21:00
+
+// DateTime
+i18nService.formatDateTime(dateToFormat);                            // 26/11/1990 23:21
+i18nService.formatDateTime(dateToFormat, i18nService.formats.SHORT); // 26/11/1990 23:21
+i18nService.formatDateTime(dateToFormat, i18nService.formats.LONG);  // 26 novembre 1990 23:21:00
+```
---
+++ js-i18n-library.js
@@ -0,0 +1,104 @@
+(function () {
+  var i18nServiceFactory = function (moment, numbro, config) {
+    var _config = {
+      referenceTimezone: 'Europe/Paris',
+      targetTimezone: 'Europe/Paris',
+      locale: 'fr-FR',
+      shortDateFormat: 'DD/MM/YYYY',
+      longDateFormat: 'LL',
+      shortTimeFormat: 'HH:mm',
+      longTimeFormat: 'HH:mm:ss'
+    };
+
+    var _parseDateTime = function (dateTimeString) {
+      var parsedDateTime = moment.tz(
+        dateTimeString,
+        moment.ISO_8601,
+        _config.referenceTimezone);
+      if (!parsedDateTime.isValid()) {
+        throw 'Bad date format for input "' + dateTimeString + '", expect ISO_8601 format.';
+      }
+
+      var timeZonedDateTime = moment.tz(parsedDateTime, _config.targetTimezone);
+      return timeZonedDateTime.locale(_config.locale);
+    };
+
+    var _mergeConfiguration = function (baseConfig, overrideConfig) {
+      var stampConfig = JSON.parse(JSON.stringify(baseConfig));
+      for (var key in overrideConfig) {
+        stampConfig[key] = overrideConfig[key];
+      }
+
+      return stampConfig;
+    };
+
+    if (!!config) {
+      _config = _mergeConfiguration(_config, config);
+    }
+
+    return {
+      formatDateTime: function (dateTimeString, formatType) {
+        var timeZonedDateTime = _parseDateTime(dateTimeString);
+        var dateFormat = _config.shortDateFormat;
+        var timeFormat = _config.shortTimeFormat;
+        if (formatType === this.formats.LONG) {
+          dateFormat = _config.longDateFormat;
+          timeFormat = _config.longTimeFormat;
+        }
+
+        return timeZonedDateTime.format(dateFormat + ' ' + timeFormat);
+      },
+      formatDate: function (dateTimeString, formatType) {
+        var timeZonedDateTime = _parseDateTime(dateTimeString);
+        var dateFormat = _config.shortDateFormat;
+        if (formatType === this.formats.LONG) {
+          dateFormat = _config.longDateFormat;
+        }
+
+        return timeZonedDateTime.format(dateFormat);
+      },
+      formatTime: function (dateTimeString, formatType) {
+        var timeZonedDateTime = _parseDateTime(dateTimeString);
+        var timeFormat = _config.shortTimeFormat;
+        if (formatType === this.formats.LONG) {
+          timeFormat = _config.longTimeFormat;
+        }
+
+        return timeZonedDateTime.format(timeFormat);
+      },
+      formatCurrency: function (value, decimalCount) {
+        var oldLanguage = numbro.language();
+        numbro.language(_config.locale);
+        if (decimalCount === undefined) {
+          decimalCount = 2;
+        }
+
+        var decimalPattern = Array(decimalCount + 1).join('0');
+        var valueFormated = numbro(value).formatCurrency('0,0[.]' + decimalPattern);
+        numbro.language(oldLanguage);
+        return valueFormated;
+      },
+      formats: {
+        SHORT: "SHORT",
+        LONG: "LONG"
+      }
+    };
+  };
+
+  // Node
+  if (typeof module !== 'undefined' && module.exports) {
+    var numbro = require("numbro"),
+      moment = require("moment");
+    module.exports = function (config) {
+      return i18nServiceFactory(moment, numbro, config);
+    };
+  }
+
+  // Browser
+  if (typeof window !== 'undefined') {
+    window.iadvize = window.iadvize || {};
+    window.iadvize.i18nServiceFactory = function (config) {
+      return i18nServiceFactory(window.moment, window.numbro, config);
+    };
+  }
+})();
